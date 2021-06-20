a9452177f014ca12d332ea38a3b39dbfc74f4626
punk1u <punk1u@protonmail.com>
2021-06-20 20:55:39 +0800

    分析MyBatis中作为缓存的key的CacheKey对象的源码

--- src/main/java/org/apache/ibatis/cache/CacheKey.java
+++ src/main/java/org/apache/ibatis/cache/CacheKey.java
@@ -23,6 +23,10 @@ import java.util.StringJoiner;
 import org.apache.ibatis.reflection.ArrayUtil;
 
 /**
+ * 即使是对于同一个语句的缓存来说，也会因为传入的参数值的不一样而导致查出来的结果不一样，
+ * 所以缓存的key-value对中的key应该涵盖运行时参数，除此之外，如果进行分页查询，查询结果
+ * 也会不同，因此key也应该涵盖分页参数。所以使用了能同时涵盖这些影响查询结果因子的CacheKey
+ * 对象作为缓存的key
  * @author Clinton Begin
  */
 public class CacheKey implements Cloneable, Serializable {
@@ -45,12 +49,27 @@ public class CacheKey implements Cloneable, Serializable {
   private static final int DEFAULT_MULTIPLIER = 37;
   private static final int DEFAULT_HASHCODE = 17;
 
+  /**
+   * 乘子，默认为37
+   */
   private final int multiplier;
+  /**
+   * CacheKey的hashCode，综合了各种影响因子
+   */
   private int hashcode;
+  /**
+   * 校验和
+   */
   private long checksum;
+  /**
+   * 影响因子个数
+   */
   private int count;
   // 8/21/2017 - Sonarlint flags this as needing to be marked transient. While true if content is not serializable, this
   // is not always true and thus should not be marked transient.
+  /**
+   * 影响因子集合
+   */
   private List<Object> updateList;
 
   public CacheKey() {
@@ -69,15 +88,34 @@ public class CacheKey implements Cloneable, Serializable {
     return updateList.size();
   }
 
+  /**
+   * 每当执行更新操作时，表示有新的影响因子参与计算
+   * @param object
+   */
   public void update(Object object) {
     int baseHashCode = object == null ? 1 : ArrayUtil.hashCode(object);
 
+    /**
+     * 自增count
+     */
     count++;
+    /**
+     * 计算校验和
+     */
     checksum += baseHashCode;
+    /**
+     * 更新baseHashCode
+     */
     baseHashCode *= count;
 
+    /**
+     * 计算hashCode
+     */
     hashcode = multiplier * hashcode + baseHashCode;
 
+    /**
+     * 保存影响因子
+     */
     updateList.add(object);
   }
 
@@ -87,27 +125,54 @@ public class CacheKey implements Cloneable, Serializable {
     }
   }
 
+
+  /**
+   * CacheKey最终要作为key存入HashMap中。所以它修阿婆覆盖equals和hashCode方法
+   * @param object
+   * @return
+   */
   @Override
   public boolean equals(Object object) {
+    /**
+     * 检测是否为同一个对象
+     */
     if (this == object) {
       return true;
     }
+    /**
+     * 检测object是否为CacheKey
+     */
     if (!(object instanceof CacheKey)) {
       return false;
     }
 
+
     final CacheKey cacheKey = (CacheKey) object;
 
+    /**
+     * 检测hashCode是否相等
+     */
     if (hashcode != cacheKey.hashcode) {
       return false;
     }
+
+    /**
+     * 检测校验和是否相同
+     */
     if (checksum != cacheKey.checksum) {
       return false;
     }
+
+    /**
+     * 检测count是否相同
+     */
     if (count != cacheKey.count) {
       return false;
     }
 
+    /**
+     * 如果上面的检测都通过了，再分别对每个影响因子进行比较
+     */
     for (int i = 0; i < updateList.size(); i++) {
       Object thisObject = updateList.get(i);
       Object thatObject = cacheKey.updateList.get(i);
@@ -120,6 +185,9 @@ public class CacheKey implements Cloneable, Serializable {
 
   @Override
   public int hashCode() {
+    /**
+     * 返回hashCode变量
+     */
     return hashcode;
   }
 
